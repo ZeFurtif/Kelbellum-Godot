0bd3c747d4f652e6abd5485aa21a882487d2eb2a
Eliott Vallot <zefurtifpro@gmail.com>
2024-06-29 15:40:22 +0200

    [EVA] Added Camera
    Now camera scales and moves to the players average position

--- Scenes/player_manager/PlayerManager.gd
+++ Scenes/player_manager/PlayerManager.gd
@@ -2,6 +2,13 @@ extends Node3D
 
 @export_file("*.tscn") var player_scene: String = "res://scenes/player/Player.tscn"
 
+var players : Dictionary = {
+	0 : null,
+	1 : null,
+	2 : null,
+	3 : null,
+}
+
 func _ready() -> void:
 	spawn_players()
 	return
@@ -9,7 +16,9 @@ func _ready() -> void:
 func spawn_players() -> void:
 	var player = load(player_scene)
 	for player_id in range(0, 4):
-		if ControllerManager.players_controller_id[player_id] != -1:
+		if ControllerManager.players_controller_id[player_id] != 0:
 			var player_copy = player.instantiate()
+			players[player_id] = player_copy
 			player_copy.PLAYER_ID = player_id
 			call_deferred("add_child", player_copy)
+	return
